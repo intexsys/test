e53a5b2d684e7805640d441da1122fcae532c13c
Konstantin Tjuterev <kostja@Konstantin-Tjuterevs-MacBook-Pro.local>
2012-01-05 00:37:54 +0200

    Fixed comments from CR, added dumb dummy

---
+++ src/Dummy.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: kostja
+ * Date: 05/01/12
+ * Time: 00:36
+ * To change this template use File | Settings | File Templates.
+ */
+class Dummy
+{
+    function beDumb()
+    {
+        echo 'I\'m dumb!';
+    }
+}
